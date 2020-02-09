4a2632cc27b92ad9be4abec2929e7b34a918c7a7
TatsukiIshijima <tatsuki445@gmail.com>
2020-02-09 10:49:37 +0900

    カメラの権限チェックまで実装

--- app/src/main/java/com/io/tatsuki/otoshidamachallenge/PermissionCheckFragment.kt
+++ app/src/main/java/com/io/tatsuki/otoshidamachallenge/PermissionCheckFragment.kt
@@ -1,28 +1,49 @@
 package com.io.tatsuki.otoshidamachallenge
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class PermissionFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_permission_check, container, false)
+    companion object {
+        private const val PERMISSIONS_REQUEST_CODE = 10
+        private val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.CAMERA)
+
+        fun hasPermissions(context: Context) = PERMISSIONS_REQUIRED.all {
+            ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+        }
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (hasPermissions(requireContext())) {
+            findNavController().navigate(R.id.goToCameraToPermissionCheck)
+        } else {
+            requestPermissions(PERMISSIONS_REQUIRED, PERMISSIONS_REQUEST_CODE)
+        }
+
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            if (PackageManager.PERMISSION_GRANTED == grantResults.firstOrNull()) {
+                findNavController().navigate(R.id.goToCameraToPermissionCheck)
+            } else {
+                // TODO:ダイアログ表示して、アプリ終了
+                Toast.makeText(context, "Permission request denied", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
