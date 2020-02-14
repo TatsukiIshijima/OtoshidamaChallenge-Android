b1e3f864f1b7b2ab9e0716da0ca7241901ddbf3a
TatsukiIshijima <tatsuki445@gmail.com>
2020-02-14 18:01:55 +0900

    メニューを非表示に設定

--- app/src/main/java/com/io/tatsuki/otoshidamachallenge/MainActivity.kt
+++ app/src/main/java/com/io/tatsuki/otoshidamachallenge/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.io.tatsuki.otoshidamachallenge
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,6 +12,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
     }
 
+    /*
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -29,4 +28,5 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+     */
 }
