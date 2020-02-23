7829654d1018fd05a3990954ab466b98a58ab7b3
TatsukiIshijima <tatsuki445@gmail.com>
2020-02-23 13:58:23 +0900

    設定画面追加

--- app/src/main/java/com/io/tatsuki/otoshidamachallenge/MainActivity.kt
+++ app/src/main/java/com/io/tatsuki/otoshidamachallenge/MainActivity.kt
@@ -1,36 +1,36 @@
 package com.io.tatsuki.otoshidamachallenge
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navController = findNavController(R.id.nav_host_fragment)
+        navController.addOnDestinationChangedListener(this)
         val appBarConfiguration = AppBarConfiguration(navController.graph)
         toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        settingsButton.setOnClickListener {
+            findNavController(R.id.nav_host_fragment).navigate(R.id.goToSettingsFromCamera)
+        }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val navController = findNavController(R.id.nav_host_fragment)
-        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: Bundle?
+    ) {
+        toolbar.isVisible = destination.id != R.id.permissionFragment
+        settingsButton.isVisible = destination.id != R.id.settingsFragment
     }
 }
---
+++ app/src/main/java/com/io/tatsuki/otoshidamachallenge/SettingsViewModel.kt
@@ -0,0 +1,7 @@
+package com.io.tatsuki.otoshidamachallenge
+
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
