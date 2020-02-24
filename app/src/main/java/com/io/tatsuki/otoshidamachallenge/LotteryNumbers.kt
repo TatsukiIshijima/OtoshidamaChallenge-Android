e6aacdd8ba8c0643774dcd74b600db2e72fabbc3
TatsukiIshijima <tatsuki445@gmail.com>
2020-02-24 21:26:27 +0900

    データ定義

---
+++ app/src/main/java/com/io/tatsuki/otoshidamachallenge/LotteryNumbers.kt
@@ -0,0 +1,17 @@
+package com.io.tatsuki.otoshidamachallenge
+
+data class LotteryNumbers(
+    val specialPrimaryForward: String = "",
+    val specialPrimaryBackward: String = "",
+    val specialSecondaryForward: String = "",
+    val specialSecondaryBackward: String = "",
+    val specialTertiaryForward: String = "",
+    val specialTertiaryBackward: String = "",
+    val specialQuaternaryForward: String = "",
+    val specialQuaternaryBackward: String = "",
+    val specialQuinaryForward: String = "",
+    val specialQuinaryBackward: String = "",
+    val firstClass: String = "",
+    val secondClass: String = "",
+    val thirdClass: String = ""
+)
