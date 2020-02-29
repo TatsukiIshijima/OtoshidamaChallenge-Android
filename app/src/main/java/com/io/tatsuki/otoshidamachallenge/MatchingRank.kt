4a8b7b7b78cb2783d59bf526ce8ac2b55f3fd182
TatsukiIshijima <tatsuki445@gmail.com>
2020-02-29 21:36:30 +0900

    検索処理追加

---
+++ app/src/main/java/com/io/tatsuki/otoshidamachallenge/MatchingRank.kt
@@ -0,0 +1,10 @@
+package com.io.tatsuki.otoshidamachallenge
+
+enum class MatchingRank {
+    FIRST,
+    SECOND,
+    THIRD,
+    SPECIAL_PRIMARY,
+    SPECIAL_SECONDARY,
+    NONE
+}
--- app/src/main/java/com/io/tatsuki/otoshidamachallenge/View/Camera/CameraViewModel.kt
+++ app/src/main/java/com/io/tatsuki/otoshidamachallenge/View/Camera/CameraViewModel.kt
@@ -1,10 +1,12 @@
 package com.io.tatsuki.otoshidamachallenge.View.Camera
 
 import androidx.lifecycle.*
-import com.io.tatsuki.otoshidamachallenge.Repository.ILotteryNumbersRepository
+import com.io.tatsuki.otoshidamachallenge.MatchingRank
+import com.io.tatsuki.otoshidamachallenge.Repository.LotteryNumbersRepository
+import kotlinx.coroutines.launch
 
 class CameraViewModel(
-    private val repository: ILotteryNumbersRepository
+    private val repository: LotteryNumbersRepository
 ) : ViewModel() {
 
     val classNumberAnalyzeResult = MutableLiveData<String>()
@@ -18,10 +20,54 @@ class CameraViewModel(
             val classNumberText = classNumberAnalyzeResult.value ?: ""
             val lotteryNumberText = lotteryNumberAnalyzeResult.value ?: ""
             if (classNumberText.isNotEmpty() && lotteryNumberText.isNotEmpty()) {
-                _combinedAnalyzeResult.value = "${classNumberText}${lotteryNumberText}"
+                _combinedAnalyzeResult.value = "${classNumberText}-${lotteryNumberText}"
             }
         }
         _combinedAnalyzeResult.addSource(classNumberAnalyzeResult, analyzeObserver)
         _combinedAnalyzeResult.addSource(lotteryNumberAnalyzeResult, analyzeObserver)
+    }
+
+    fun getLotteryNumbers() {
+        viewModelScope.launch {
+            repository.loadLotteryNumbers()
+        }
+    }
+
+    fun matchLotteryNumbers(analyzedText: String): MatchingRank {
+        val savedLotteryNumbers =
+            repository.cacheLotteryNumbers ?: return MatchingRank.NONE
+        if (analyzedText.length <= 9) {
+            return MatchingRank.NONE
+        }
+        val specialClassPrimaryTarget = analyzedText.substring(analyzedText.length - 10)
+        if ((specialClassPrimaryTarget == "${savedLotteryNumbers.specialPrimaryForward}${savedLotteryNumbers.specialPrimaryBackward}") ||
+            (specialClassPrimaryTarget == "${savedLotteryNumbers.specialSecondaryForward}${savedLotteryNumbers.specialSecondaryBackward}") ||
+            (specialClassPrimaryTarget == "${savedLotteryNumbers.specialTertiaryForward}${savedLotteryNumbers.specialTertiaryBackward}") ||
+            (specialClassPrimaryTarget == "${savedLotteryNumbers.specialQuaternaryForward}${savedLotteryNumbers.specialQuaternaryBackward}")) {
+            return MatchingRank.SPECIAL_PRIMARY
+        }
+
+        val specialClassSecondaryTarget = analyzedText.substring(analyzedText.length - 7)
+        if (specialClassSecondaryTarget == "${savedLotteryNumbers.specialQuinaryForward}${savedLotteryNumbers.specialQuinaryBackward}") {
+            return MatchingRank.SPECIAL_SECONDARY
+        }
+
+        val firstClassTarget = analyzedText.substring(analyzedText.length - 6)
+        if (firstClassTarget == savedLotteryNumbers.firstClass) {
+            return MatchingRank.FIRST
+        }
+
+        val secondClassTarget = analyzedText.substring(analyzedText.length - 4)
+        if (secondClassTarget == savedLotteryNumbers.secondClass) {
+            return MatchingRank.SECOND
+        }
+
+        val thirdClassTarget = analyzedText.substring(analyzedText.length - 2)
+        if (thirdClassTarget == savedLotteryNumbers.thirdClassNumberPrimary ||
+            thirdClassTarget == savedLotteryNumbers.thirdClassNumberSecondary ||
+            thirdClassTarget == savedLotteryNumbers.thirdClassNumberTertiary) {
+            return MatchingRank.THIRD
+        }
+        return MatchingRank.NONE
     }
 }
